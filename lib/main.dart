ad64a15495b61d65a3c083383c5aa82f74c976fb
vemarav <vemaravv@gmail.com>
2018-09-07 11:24:41 +0530

    update: Export Image completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
+import 'dart:async';
+import 'dart:typed_data';
 
 void main() {
   runApp(
@@ -19,16 +21,45 @@ class SignApp extends StatefulWidget {
 
 class SignAppState extends State<SignApp> {
   GlobalKey<SignatureState> signatureKey = GlobalKey();
+  var image;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Signature(key: signatureKey),
       persistentFooterButtons: <Widget>[
-        OutlineButton(onPressed: () {
-          
-        })
+        FlatButton(
+          child: Text('Clear'),
+          onPressed: () {
+            signatureKey.currentState.clearPoints();
+          },
+        ),
+        FlatButton(
+          child: Text('Save'),
+          onPressed: () {
+            // Future will resolve later
+            // so setState @image here and access in #showImage
+            // to avoid @null Checks
+            setState(() {
+              image = signatureKey.currentState.rendered;
+            });
+            showImage(context);
+          },
+        )
       ],
+    );
+  }
+
+  Future<Null> showImage(BuildContext context) async {
+    var pngBytes = await image.toByteData(format: ui.ImageByteFormat.png);
+    // Use plugin [path_provider] to export image to storage
+    return showDialog<Null>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Image.memory(Uint8List.view(pngBytes.buffer)),
+        );
+      }
     );
   }
 }
@@ -43,14 +74,22 @@ class Signature extends StatefulWidget {
 }
 
 class SignatureState extends State<Signature> {
+  // [SignatureState] responsible for receives drag/touch events by draw/user
+  // @_points stores the path drawn which is passed to
+  // [SignaturePainter]#contructor to draw canvas
   List<Offset> _points = <Offset>[];
 
   ui.Image get rendered {
+    // [CustomPainter] has its own @canvas to pass our
+    // [ui.PictureRecorder] object must be passed to [Canvas]#contructor
+    // to capture the Image. This way we can pass @recorder to [Canvas]#contructor
+    // using @painter[SignaturePainter] we can call [SignaturePainter]#paint
+    // with the our newly created @canvas
     ui.PictureRecorder recorder = ui.PictureRecorder();
     Canvas canvas = Canvas(recorder);
     SignaturePainter painter = SignaturePainter(points: _points);
-
     var size = context.size;
+    painter.paint(canvas, size);
     return recorder.endRecording()
         .toImage(size.width.floor(), size.height.floor());
   }
@@ -80,9 +119,23 @@ class SignatureState extends State<Signature> {
       ),
     );
   }
+
+  // clearPoints method used to reset the canvas
+  // method can be called using
+  //   key.currentState.clearPoints();
+  void clearPoints() {
+    setState(() {
+      _points.clear();
+    });
+  }
 }
 
+
 class SignaturePainter extends CustomPainter {
+  // [SignaturePainter] receives points through constructor
+  // @points holds the drawn path in the form (x,y) offset;
+  // This class responsible for drawing only
+  // It won't receive any drag/touch events by draw/user.
   List<Offset> points = <Offset>[];
 
   SignaturePainter({this.points});
